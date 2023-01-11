7820fb63ab8bb44b5ed5b74aeced7f3a2695907d
jamiepollock <jamie.pollock@rhythmagency.com>
2023-01-11 14:29:09 -0500

    Added xml-docs to IPageModel.

--- src/Rhythm.Models.Common/IPageModel.cs
+++ src/Rhythm.Models.Common/IPageModel.cs
@@ -1,6 +1,12 @@
 ﻿namespace Rhythm.Models.Common;
 
+/// <summary>
+/// An interface for creating a page.
+/// </summary>
 public interface IPageModel : IHaveComponents
 {
+    /// <summary>
+    /// Gets the page content.
+    /// </summary>
     IReadOnlyCollection<IComponentModel> Content { get; }
 }
