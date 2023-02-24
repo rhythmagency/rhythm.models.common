d6158061734f1d75f2a1b24a6715ad3733846581
jamiepollock <jamie.pollock@rhythmagency.com>
2023-02-24 12:56:33 -0500

    Added Generic model versions
    IPageModel and IHavePageComponents now support generic versions

--- src/Rhythm.Models.Common/IHavePageComponents.cs
+++ src/Rhythm.Models.Common/IHavePageComponents.cs
@@ -5,12 +5,20 @@ using System.Collections.Generic;
 /// <summary>
 /// A contract for classes that have page components.
 /// </summary>
-public interface IHavePageComponents
+public interface IHavePageComponents : IHavePageComponents<IPageComponentModel>
+{
+}
+
+/// <summary>
+/// A contract for classes that have page components.
+/// </summary>
+/// <typeparam name="TComponent">The type of <see cref="IPageComponentModel" />.</typeparam>
+public interface IHavePageComponents<TComponent> where TComponent : class, IPageComponentModel
 {
     /// <summary>
     /// Gets the page components of this class.
     /// </summary>
     /// <returns>A collection of page components</returns>
     /// <remarks>If this class is also a <see cref="IPageComponentModel"/> and should be returned by <see cref="FlattenExtensions.Flatten(IEnumerable{IPageComponentModel})"/> or <see cref="FlattenExtensions.Flatten(IHavePageComponents?)"/> then it should be included here.</remarks>
-    IReadOnlyCollection<IPageComponentModel> GetPageComponents();
+    IReadOnlyCollection<TComponent> GetPageComponents();
 }
--- src/Rhythm.Models.Common/IPageModel.cs
+++ src/Rhythm.Models.Common/IPageModel.cs
@@ -3,10 +3,18 @@
 /// <summary>
 /// An interface for creating a page.
 /// </summary>
-public interface IPageModel : IHavePageComponents
+public interface IPageModel : IPageModel<IPageComponentModel>
+{
+}
+
+/// <summary>
+/// An interface for creating a page.
+/// </summary>
+/// <typeparam name="TComponent">The type of <see cref="IPageComponentModel" />.</typeparam>
+public interface IPageModel<TComponent> : IHavePageComponents<TComponent> where TComponent : class, IPageComponentModel
 {
     /// <summary>
     /// Gets the page content.
     /// </summary>
-    IReadOnlyCollection<IPageComponentModel> Content { get; }
+    IReadOnlyCollection<TComponent> Content { get; }
 }
